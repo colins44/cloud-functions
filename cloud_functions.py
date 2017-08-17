f986ac857ee3a3e53ca4e3d43191b4b38ff44fc1
Colin Pringle-Wood <colin.pringle-wood@aussiecommerce.com.au>
2017-08-17 18:00:24 +1000

    adding db connection

--- cloud_functions.py
+++ cloud_functions.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sqlalchemy
+
 
 def add(x, y):
     return x + y
@@ -22,3 +24,24 @@ def log_something():
     logger.exception('this is the exception message')
     logger.warning('this is the warning message')
     logger.critical('this is the critical message')
+
+
+def connect(user, password, db, host='localhost', port=5432):
+    '''Returns a connection and a metadata object'''
+    # We connect with the help of the PostgreSQL URL
+    # postgresql://federer:grandestslam@localhost:5432/tennis
+    url = 'postgresql://{}:{}@{}:{}/{}'
+    url = url.format(user, password, host, port, db)
+
+    # The return value of create_engine() is our connection object
+    con = sqlalchemy.create_engine(url, client_encoding='utf8')
+
+    # We then bind the connection to MetaData()
+    meta = sqlalchemy.MetaData(bind=con, reflect=True)
+    if con:
+        con = 'connected'
+    if meta:
+        meta = 'meta'
+
+    return con, meta
+
